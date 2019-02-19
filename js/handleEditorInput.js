8fccb6e6043cc23f348ee73b1c7c41e58f9709f7
Scott Roberts <s.e.roberts0@gmail.com>
2019-02-19 12:06:18 -0800

    Add styling to content

--- js/handleEditorInput.js
+++ js/handleEditorInput.js
@@ -3,9 +3,15 @@
  * @param {Event} event
  */
 export default function handleEditorInput(event) {
+  const textElement = event.currentTarget.parentNode.querySelector('.text-element');
   if (event.target.type === 'text' || event.target.tagName === 'TAGNAME') {
-    const textElement = event.currentTarget.parentNode.querySelector('.text-element');
     const text = event.target.value;
     textElement.innerText = text;
+  } else {
+    const value = event.target.value;
+    const styleName = event.target.name;
+    const unit = event.target.dataset.unit || '';
+    // handle the styles
+    textElement.style[styleName] = `${value}${unit}`;
   }
 }
